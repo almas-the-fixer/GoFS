1553b44b7ce03e5f0fee3abe62bc0542ee6a9f7b
almas-the-fixer <professionalhacker74001@gmail.com>
2026-07-18 15:52:02 +0530

    added /users route to get all users and refactored main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,43 @@
-package main
+	package main
 
-import (
-	"log"
-	"github.com/gofiber/fiber/v3"
-)
+	import (
+		"context"
+		"fmt"
+		"log"
 
-func main() {
-	app := fiber.New()
+		"gofs/internal/database"
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+		"github.com/gofiber/fiber/v3"
+		"github.com/joho/godotenv"
+	)
 
-	app.Get("/health", func(c fiber.Ctx) error {
-		return c.JSON(fiber.Map{"status": "healthy"})
-	})
+	func main() {
+		app := fiber.New()
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("AN ERROR OCCURED LOADING THE ENV VARIABLES: ", err)
+		}
 
-	log.Fatal(app.Listen(":3000"))
-}
+		conn , err := database.ConnectDB()
+		if err != nil {
+			log.Fatal("AN ERROR OCCURED WHEN CONNECTING TO DB: ", err)
+		}
+		
+		defer conn.Close(context.Background())
+
+
+
+		app.Get("/", func(c fiber.Ctx) error {
+			return c.SendString("Hello, World!")
+		})
+
+		app.Get("/users", func (c fiber.Ctx) error {
+			users := database.GetUsers(conn)
+			return c.JSON(users)
+		})
+
+		app.Get("/health", func(c fiber.Ctx) error {
+			return c.JSON(fiber.Map{"status": "healthy"})
+		})
+
+		log.Fatal(app.Listen(":3000"))
+	}
