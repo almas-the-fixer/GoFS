8ed47fc31bc047b0b032be063a830a78321cde35
almas-the-fixer <professionalhacker74001@gmail.com>
2026-07-24 17:32:02 +0530

    Added a Demp /me Route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gofs/handlers"
+	"gofs/internal/middleware"
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/jackc/pgx/v5"
@@ -18,10 +19,17 @@ func RegisterRoutes(app *fiber.App, conn *pgx.Conn) {
 
 	// User CRUD Need To Refactor Later as Admin / User Endpoints...
 	app.Get("/user/:id", handlers.GetUser(conn))
-	app.Get("/users", handlers.GetUsers(conn))
+	app.Get("/users", middleware.Protect(), handlers.GetUsers(conn))
 	app.Delete("/users/:id", handlers.DeleteUser(conn))
 	app.Post("/register", handlers.RegisterUser(conn))
 
 	// Auth Routes for User
 	app.Post("/login", handlers.Login(conn))
+
+	//Middleware Routes test
+	app.Get("/me", middleware.Protect(), func(req fiber.Ctx) error {
+		userID := req.Locals("user_id")
+		email := req.Locals("email")
+		return req.Status(200).JSON(fiber.Map{"user_id": userID, "email": email})
+	})
 }
