13495f8dc151b7a2c4473ce5cdfa957cb862c9d6
almas-the-fixer <professionalhacker74001@gmail.com>
2026-07-24 14:53:15 +0530

    Refactored auth_handlers

--- handlers/auth_handlers.go
+++ handlers/auth_handlers.go
@@ -4,12 +4,41 @@ import (
 	"fmt"
 	"gofs/internal/database"
 	"gofs/internal/types"
+	"gofs/internal/validation"
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/jackc/pgx/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Create a User
+func RegisterUser(conn *pgx.Conn) fiber.Handler {
+	return func(req fiber.Ctx) error {
+		user := new(types.UserCreateRequest)
+		if err := req.Bind().Body(user); err != nil {
+			return fiber.ErrBadRequest
+		}
+		err := validation.UserCreateRequestValidator(*user)
+		if err != nil {
+			return err
+		}
+		// Hashing Password Before Storing it
+		hashedPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			fmt.Println("An Error Occures while HASHING password: ", err)
+			return err
+		}
+		
+		// Need to change this because its mutating the fields and user.Password now contains hashedPassword, that can be confusing so later on needs refactor
+		user.Password = string(hashedPass)
+		id, err := database.InsertUser(conn, *user)
+		if err != nil {
+			fmt.Println("An Error Occured: ", err)
+			return req.Status(401).JSON(fiber.Map{"status": "Bad request"})
+		}
+		return req.Status(201).JSON(fiber.Map{"status": "user created successfully", "user_id": id})
+	}
+}
 
 func Login(conn *pgx.Conn) fiber.Handler{
 	return func(req fiber.Ctx) error{
@@ -36,4 +65,4 @@ func Login(conn *pgx.Conn) fiber.Handler{
 		}
 		return req.Status(200).JSON(fiber.Map{"status": "login successfull!"})
 	}
-} 
+}
